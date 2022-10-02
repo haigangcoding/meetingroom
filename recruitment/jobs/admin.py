d292dfb5125feef4cbc771e20ad6d2cbed0c5f15
haigangshushu@gmail.com <haigangshushu@gmail.com>
2022-10-02 22:11:25 +0800

    快速迭代完善应用

--- recruitment/jobs/admin.py
+++ recruitment/jobs/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from jobs.models import Job
 # Register your models here.
 
+# 引入管理类的参数
+class JobAdmin(admin.ModelAdmin):
+    # 隐藏创建人 创建时间
+
+    # 列表展示的参数
+    list_display = ('job_name', 'job_type', 'job_city', 'created_date', 'modified_date')
+    pass
+
+
 admin.site.register(Job)
