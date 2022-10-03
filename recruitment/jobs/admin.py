b0723ec119e80ee2d05f781d2913897e84d9a1cb
haigangshushu@gmail.com <haigangshushu@gmail.com>
2022-10-03 23:18:40 +0800

    快速迭代完善应用

--- recruitment/jobs/admin.py
+++ recruitment/jobs/admin.py
@@ -4,11 +4,18 @@ from jobs.models import Job
 
 # 引入管理类的参数
 class JobAdmin(admin.ModelAdmin):
-    # 隐藏创建人 创建时间
-
+    # 隐藏创建人 创建时间 修改时间
+    exclude = ('creator', 'created_date', 'modified_date')
     # 列表展示的参数
-    list_display = ('job_name', 'job_type', 'job_city', 'created_date', 'modified_date')
-    pass
+    list_display = ('job_name', 'job_type', 'job_city', 'creator', 'created_date', 'modified_date')
+    # 利益 ModelAdmin 这个父类里面定义的方法
+    def save_model(self, request, obj, form, change):
+
+        # 把职位的创建人设置为当前的用户, 就可以把当前登录的用户设置成这个 Model 的创建人
+        obj.creator = request.user
+        # 调用 父类的方法来保存我们的对象
+        super().save_model(request, obj, form, change)
 
 
-admin.site.register(Job)
+
+admin.site.register(Job, JobAdmin)
