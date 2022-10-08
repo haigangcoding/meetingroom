410998293e403aeb88639d20b47299a78d2bfacd
haigangshushu@gmail.com <haigangshushu@gmail.com>
2022-10-08 22:38:50 +0800

    添加自定义页面：让匿名用户可以浏览职位列表页

--- recruitment/jobs/views.py
+++ recruitment/jobs/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from jobs.models import Job
+from jobs.models import Cities, JobTypes
+
+# Django 视图 有几种方法： 可以用函数去定义，可以用视图的类去定义
+# 用函数去定义在 views 层里面
+
+def joblist(request):
+    job_list = Job.objects.order_by('job_type')
+    # 加载模板 等于用 loader 模板的加载器来加载 get_template('joblist.html')
+    template = loader.get_template('joblist.html')
+    # 定义上下文
+    context = {'job_list': job_list}
+
+    # 遍历职位列表
+    for job in job_list:
+        job.city_name = Cities[job.job_city]
+        job.job_type = JobTypes()
+
